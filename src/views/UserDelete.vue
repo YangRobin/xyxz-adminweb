1a6fd6f6831b730992004adfa49c6e3b42477f05
zgzygf <2481162091@qq.com>
2018-12-24 14:54:53 +0800

    scss样式增加

--- src/views/UserDelete.vue
+++ src/views/UserDelete.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
      user delete page
+     <div class="container">
+     <div class="row">
+        <div class="col-lg-8 col-md-12 col-sm-2">asdfsd</div>
+        <div class="col-lg-16 col-md-24 col-sm-12">45455</div>
+     </div>
+     </div>
   </div>
 </template>
