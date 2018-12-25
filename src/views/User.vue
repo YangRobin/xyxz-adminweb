27e171a14eda58f36a7ad75a83be8d2d3dac495b
zgzygf <2481162091@qq.com>
2018-12-25 17:37:37 +0800

    user

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="about">
     <div class="paths">
-     <h3>用户管理</h3>
+      <fieldset class="zgz-title-field">
+        <legend>用户管理</legend>
+      </fieldset>
       <ul>
-        <li>
-          <router-link to="/user/user-survey">
+        <li >
+          <router-link to="/user/user-survey" >
             <i class="iconfont icon-check-circle"></i>用户统计
           </router-link>
         </li>
-        <li>
+        <li class="active">
           <router-link to="/user/add-user">
             <i class="iconfont icon-check-circle"></i>新增用户
           </router-link>
@@ -28,13 +30,45 @@
 .about {
   .paths {
     background-color: #fff;
+     .zgz-title-field{
+      margin: 10px 0 20px;
+      border-style: solid;
+      border-width: 1px 0 0;
+      legend{
+        margin-left: 20px;
+        padding: 0 10px;
+        font-size: 20px;
+        font-weight: 300;
+      }
+    }
     ul {
       overflow: hidden;
+      background: #ccc;
+      border-top: #fff;
       li {
         float: left;
-        margin-right: 10px;
-        border-right: 2px solid #c028c0;
         padding: 10px;
+        transition: all 0.3s;
+        &:hover{
+          background: #fff;
+           border-color: #c028c0;
+          color: #c028c0;
+          border-top: 1px solid;
+        }
+        a{
+          display: block;
+          padding: 0 10px;
+          text-decoration: none;
+          i{
+            margin-right:5px;
+          }  
+        }
+      }
+      .active{
+          background: #fff;
+          border-color: #c028c0;
+          color: #c028c0;
+          border-top: 1px solid;
       }
     }
   }
